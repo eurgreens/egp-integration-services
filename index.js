51df51c48ebbcd42d49fd051fe1c4330c6d9dd3d
Rodrigo Leon <rodrigo@tectonica.co>
2023-11-01 11:30:34 -0300

    Fixes on pagination by Fer

--- index.js
+++ index.js
@@ -217,66 +217,121 @@ app.get('/motion-tools', async (req, res) => {
   const motionToolLists = results.lists
     .filter((item) => item.name.includes('Motion tool'))
     .map((item) => ({ name: item.name, id: item.listId }));
+    
   for (const list of motionToolLists) {
     let usersValues = [];
-    const listValues = await fetch(`https://api.hubapi.com/contacts/v1/lists/${list.id}/contacts/all`, {
+
+    /*
+    const listValues = await fetch(`https://api.hubapi.com/contacts/v1/lists/${list.id}/contacts/all?count=100`, {
       headers: {
         Authorization: `Bearer ${process.env.AUTH}`,
       },
     });
-    const listValuesResponse = await listValues.json();
 
-    // const usersValues = listValuesResponse.contacts.map(item => {
-    for (const item of listValuesResponse.contacts) {
+    const listValuesResponse = await listValues.json();
+    */
+
+    let offset = 0;
+    const allContacts = [];
+        
+    async function fetchContacts() {
+      try {
+            while (true) {
+              const response = await fetch(`https://api.hubapi.com/contacts/v1/lists/${list.id}/contacts/all?count=100&vidOffset=${offset}`, {
+                headers: {
+                Authorization: `Bearer ${process.env.AUTH}`,
+                },
+            });
+        
+              if (response.ok) {
+                const data = await response.json();
+                const contacts = data.contacts || [];
+                if (contacts.length === 0) {
+                  break;
+                }
+        
+                allContacts.push(...contacts);
+                if (offset == data['vid-offset']) {
+                    break
+                } else {
+                    offset = data['vid-offset'];
+                    console.log(offset);
+                }
+              } else {
+                console.error(`Error: ${response.status} - ${await response.text()}`);
+                break;
+              }
+            }
+        
+            // Now 'allContacts' contains all the contacts from the list
+            console.log(`Total contacts retrieved: ${allContacts.length}`);
+            return allContacts;
+      } catch (error) {
+            console.error('An error occurred:', error);
+      }
+    }
+        
+    const contacts = await fetchContacts();
+
+    //for (const item of listValuesResponse.contacts) {
+    for (const item of contacts) {
+
       let company = '';
 
-      const getContactProperties = await fetch(`https://api.hubapi.com/contacts/v1/contact/vid/${item.vid}/profile`, {
-        headers: {
-          Authorization: `Bearer ${process.env.AUTH}`,
-        },
-      });
+      try {
 
-      const getConactPropResponse = await getContactProperties.json();
-
-      const getMemberParty = await fetch(
-        `https://api.hubspot.com/crm/v3/objects/contacts/${item.vid}?associations=2-117824001`,
-        {
+        const getContactProperties = await fetch(`https://api.hubapi.com/contacts/v1/contact/vid/${item.vid}/profile`, {
           headers: {
             Authorization: `Bearer ${process.env.AUTH}`,
           },
-        }
-      );
+        });
 
-      const responseMemberParty = await getMemberParty.json();
-      if (responseMemberParty?.associations?.p26289884_member_parties) {
-        for (const party of responseMemberParty.associations.p26289884_member_parties.results) {
-          if (party.type == 'contact_to_member_parties') {
-            const getPartyName = await fetch(
-              `https://api.hubapi.com/crm/v3/objects/2-117824001/${party.id}?properties=member_party_name`,
-              {
-                headers: {
-                  Authorization: `Bearer ${process.env.AUTH}`,
-                },
-              }
-            );
-            const responsePartyName = await getPartyName.json();
-            company = responsePartyName.properties.member_party_name;
+        const getConactPropResponse = await getContactProperties.json();
+
+        const getMemberParty = await fetch(
+          `https://api.hubspot.com/crm/v3/objects/contacts/${item.vid}?associations=2-117824001`,
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.AUTH}`,
+            },
+          }
+        );
+
+        const responseMemberParty = await getMemberParty.json();
+        if (responseMemberParty?.associations?.p26289884_member_parties) {
+          for (const party of responseMemberParty.associations.p26289884_member_parties.results) {
+            if (party.type == 'contact_to_member_parties') {
+              const getPartyName = await fetch(
+                `https://api.hubapi.com/crm/v3/objects/2-117824001/${party.id}?properties=member_party_name`,
+                {
+                  headers: {
+                    Authorization: `Bearer ${process.env.AUTH}`,
+                  },
+                }
+              );
+              const responsePartyName = await getPartyName.json();
+              company = responsePartyName.properties.member_party_name;
+            }
           }
         }
-      }
-      // const hasCompany = getConactPropResponse['associated-company'].properties?.type?.value
-      // if(hasCompany){
-      //   company = getConactPropResponse['associated-company'].properties.name.value
-      // }
+        // const hasCompany = getConactPropResponse['associated-company'].properties?.type?.value
+        // if(hasCompany){
+        //   company = getConactPropResponse['associated-company'].properties.name.value
+        // }
+        
 
-      const userEmail = item['identity-profiles'][0].identities.filter((item) => item.type === 'EMAIL')[0].value;
-      usersValues.push({
-        vid: item.vid,
-        name: item.properties.firstname ? item.properties.firstname.value : '',
-        lastName: item.properties.lastname ? item.properties.lastname.value : '',
-        party: company,
-        email: userEmail,
-      });
+        const userEmail = item['identity-profiles'][0].identities.filter((item) => item.type === 'EMAIL')[0].value;
+        usersValues.push({
+          vid: item.vid,
+          name: item.properties.firstname ? item.properties.firstname.value : '',
+          lastName: item.properties.lastname ? item.properties.lastname.value : '',
+          party: company,
+          email: userEmail,
+        });
+
+      } catch (e) {
+        res.send('error');
+      }
     }
 
     const listName = list.name.split('|')[1].trim();
@@ -285,6 +340,9 @@ app.get('/motion-tools', async (req, res) => {
     listUsers.push({ listName: listName, users: usersValues });
   }
 
+  //console.log(listUsers[0].users);
+  //res.send(JSON.stringify(listUsers));
+  
   //send data to motion tool
   const sendData = await fetch('https://egp-test.discuss.green/webhook/usersync', {
     method: 'POST',
@@ -294,6 +352,8 @@ app.get('/motion-tools', async (req, res) => {
   const responseSendData = await sendData.json();
 
   res.send(JSON.stringify(responseSendData));
+  
+
 });
 
 app.post('/list-users', async (req, res) => {
